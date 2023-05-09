17f266b7b558ea53aea7ae393188054564ecfffa
wisniewski-kacper <88401564+wisniewski-kacper@users.noreply.github.com>
2023-05-09 19:36:55 +0200

    Refactor(views, components): Create modules for views and components (#4)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,2 @@
-import LoadingSpinner from './LoadingSpinner.vue'
-import ReportProductListItem from './ReportProductListItem.vue';
-import ReportListItem from './ReportListItem.vue';
-import MessageDisplay from './MessageDisplay.vue';
-
-export {LoadingSpinner, ReportProductListItem, ReportListItem, MessageDisplay};
+export {LoadingSpinner, MessageDisplay} from './common';
+export {ReportListItem, ReportProductListItem} from './report';
